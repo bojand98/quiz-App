5d12b8a7bb7c5d54d5adc9675138dfb0ffa630f7
Bojan Dimitrijevski <>
2018-10-23 20:36:44 +0200

    added stack view

---
+++ Quiz-App/Quiz-App/ViewController3.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController3.swift
+//  Quiz-App
+//
+//  Created by Bojan Dimitrijevski on 23/10/18.
+//  Copyright © 2018 Bojan Dimitrijevski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    //outlets for Buttons bellow
+  
+//actions bellow this
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
