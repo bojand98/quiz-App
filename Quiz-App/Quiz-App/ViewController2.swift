2b777e424d0a4fe1b9d3bad359397cafb606baf9
Bojan Dimitrijevski <>
2018-10-27 14:32:16 +0200

    vezbi sabota

--- Quiz-App/Quiz-App/ViewController2.swift
+++ Quiz-App/Quiz-App/ViewController2.swift
@@ -8,29 +8,6 @@
 
 import UIKit
 
-class Prasanje{
-    var prasanje: String = ""
-    var odgovor1: String = ""
-    var odgovor2: String = ""
-    var odgovor3: String = ""
-    var odgovor4: String = ""
-    var tocenOdg: String = ""
-    var moreInfo: String = ""
-    var slika: String = ""
-    
-    init(pras:String, odg1:String,odg2:String,odg3:String,odg4:String,tocen:String,moreinfo:String,sl:String) {
-        prasanje=pras
-        odgovor1=odg1
-        odgovor2=odg2
-        odgovor3=odg3
-        odgovor4=odg4
-        tocenOdg=tocen
-        moreInfo=moreinfo
-        slika=sl
-    }
-    
-}
-
 
 class ViewController2: UIViewController {
     
